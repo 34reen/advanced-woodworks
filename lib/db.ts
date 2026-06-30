6235958015ddec4755c8e9ed873c5333e219db4c
34reen <obundelaureen@gmail.com>
2026-06-30 12:41:13 +0300

    Configure Railway production database

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,28 @@
 import mysql from "mysql2/promise";
-console.log(process.env.DB_NAME);
 
 const requiredEnvVars = [
   "DB_HOST",
+  "DB_PORT",
   "DB_USER",
   "DB_NAME",
 ];
 
 requiredEnvVars.forEach((key) => {
   if (!process.env[key]) {
-    throw new Error(`Missing environment variable: ${key}`);
+    throw new Error(
+      `Missing environment variable: ${key}`
+    );
   }
 });
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
